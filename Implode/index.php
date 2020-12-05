6fe34bce7daa4f639afb8ff6dc5954907b229cea
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-12-05 23:09:20 +0800

    forget to save

--- Implode/index.php
+++ Implode/index.php
@@ -35,8 +35,37 @@ $array = [
 
 echo implode('; ', $array);
 
+var_dump(implode('',array(true, true, false, false, true)));
 
 
+# ==== quite handy in INSERT statements: ====
+// array containing data
+$array = array(
+  "name" => "John",
+  "surname" => "Doe",
+  "email" => "j.doe@intelligence.gov"
+);
+
+// build query...
+$sql  = "INSERT INTO table";
+
+// implode keys of $array...
+$sql .= " (`".implode("`, `", array_keys($array))."`)";
+
+// implode values of $array...
+$sql .= " VALUES ('".implode("', '", $array)."') ";
+
+// execute query...
+$result = mysql_query($sql) or die(mysql_error());
+
+# ==== handier if you use the following: =
+$id_nums = array(1,6,12,18,24);
+$id_nums = implode(", ", $id_nums);
+$sqlquery = "Select name,email,phone from usertable where user_id IN ($id_nums)";
+// $sqlquery becomes "Select name,email,phone from usertable where user_id IN (1,6,12,18,24)"
 
 
-
+# null values are imploded too. You can use array_filter() to sort out null values.
+$ar = array("hello", null, "world");
+print(implode(',', $ar)); // hello,,world
+print(implode(',', array_filter($ar, function($v){ return $v !== null; }))); // hello,world
